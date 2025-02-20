88ddbead9d0f18819bf98621e07cf307dd81ec80
sibbi07 <ibbihas3108@gmail.com>
2025-02-20 18:52:49 +0000

    Update tilemap.py

--- nea v3/tilemap.py
+++ nea v3/tilemap.py
@@ -1,6 +1,6 @@
 import pygame
 import json
-from game_objects import enemy_group
+from game_objects import enemy_group, Portal
 
 tile_size = 48
 ground_level = 466 
@@ -12,6 +12,7 @@ class Tilemap():
         self.enemy_pos = []
         self.window = window
         self.player = player
+        self.portals = []
 
         try:
             with open(map_file) as f:
@@ -26,7 +27,7 @@ class Tilemap():
         
         self.tileset_first_grid = self.data["tilesets"][0]["firstgid"]
         self.load_tiles()
-        self.load_objects()
+        self.load_enemies()
 
         if self.data["layers"]:
             self.tilemap_width = self.data["layers"][0]["width"] * tile_size
@@ -34,6 +35,8 @@ class Tilemap():
         else:
             self.tilemap_width = 0
             self.tilemap_height = 0
+
+        self.load_portals()
 
     def load_tiles(self):
         for layer in self.data["layers"]:
@@ -49,7 +52,7 @@ class Tilemap():
         1: "maps/individual tiles/block_(2).png",
         2: "maps/individual tiles/grass block (1).png",
         3: "maps/individual tiles/block_(3).png",
-        4: "maps/individual tiles/block_(4).png"
+        5: "maps/individual tiles/block_(4).png"
         }                        
 
         tile_path = tutorial_tile_image_map.get(tile_id - self.tileset_first_grid + 1)
@@ -61,20 +64,39 @@ class Tilemap():
 
             self.tile_list.append((image, image_rect, image_mask))
 
-    def load_objects(self):
+    def load_enemies(self):
         for layer in self.data["layers"]:
             if layer["type"] == "objectgroup" and layer["name"] == "Enemies":
                 for obj in layer["objects"]:
                     self.enemy_pos.append((obj["x"], obj["y"]))
-
-
+    
+    def load_portals(self):
+        portal_spawn_positions = []
+        for layer in self.data["layers"]:
+            if layer["type"] == "tilelayer":
+                for y in range(layer["height"]):
+                    for x in range(layer["width"]):
+                        tile_id = layer["data"][y * layer["width"] + x]
+                        if tile_id == 5:
+                            portal_x = x * tile_size
+                            portal_y = y * tile_size - tile_size
+                            portal_spawn_positions.append((portal_x, portal_y))
+        
+        for x, y in portal_spawn_positions:
+            portal = Portal(
+                x,
+                y,
+                tile_size,
+                tile_size,
+                "next_map.tmj",
+                x,
+                y,
+                "maps/individual tiles/portal frame 1.png"
+            )
+        self.portals.append(portal)
 
     def set_entities(self, player):
         self.player = player
-        player_x = 2 * tile_size
-        player_y = 39 * tile_size
-
-        self.player.rect.topleft = (player_x, player_y)
 
         from Entities_file import Enemy
         for enemy_x, enemy_y in self.enemy_pos:
@@ -84,6 +106,9 @@ class Tilemap():
     def draw(self, window, camera_x_offset, camera_y_offset):
         for img, img_rect, _ in self.tile_list:
             window.blit(img, img_rect.move(-camera_x_offset, -camera_y_offset))
+
+        for portal in self.portals:
+            portal.draw(self.window, camera_x_offset, camera_y_offset)
 
     def is_walkable(self, pos):
         x, y = pos
